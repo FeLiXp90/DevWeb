03d2f44c8093f31bcfbecc7291ef5c86d98964a8
FeLiXp90 <felipebecallitrindade@gmail.com>
2023-10-02 16:45:29 -0300

    59:57

---
+++ src/main/java/com/example/DevWeb/controllers/ProductController.java
@@ -0,0 +1,16 @@
+package com.example.DevWeb.controllers;
+
+import com.example.DevWeb.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+
+
+}
